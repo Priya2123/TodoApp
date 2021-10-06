76bf6d7d38e1baefe7a26c0d643f6f1bed4c87f9
Priya2123 <aggarwalpriya1121@gmail.com>
2021-10-06 23:29:21 +0530

    WIP

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import Colors from './Colors';
+import {AntDesign} from 'react-native-vector-icons';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -12,6 +13,13 @@ export default class App extends React.Component {
             To-do{' '}
             <Text style={{fontWeight: '300', color: Colors.blue}}>Lists</Text>{' '}
           </Text>
+          <View style={styles.divider} />
+        </View>
+        <View style={{marginVertical: 48}}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={Colors.blue} />
+          </TouchableOpacity>
+          <Text style={styles.add}>Add List</Text>
         </View>
       </View>
     );
@@ -31,4 +39,26 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lightBlue,
     alignSelf: 'center',
   },
+  title: {
+    fontSize: 38,
+    fontWeight: '800',
+    color: Colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderRadius: 4,
+    borderColor: Colors.blue,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  add: {
+    fontWeight: '600',
+    fontSize: 14,
+    color: Colors.blue,
+    marginTop: 8,
+  },
 });
+
+export default App;
