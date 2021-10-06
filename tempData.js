c8bd52197cb5d1fd842c49dc576ee10a1e7f3ee0
Priya2123 <aggarwalpriya1121@gmail.com>
2021-10-07 00:41:44 +0530

    'WIP'

---
+++ tempData.js
@@ -0,0 +1,93 @@
+/* eslint-disable prettier/prettier */
+const tempData = [
+  {
+    name: 'Sleep',
+    color: 'green',
+    todos: [
+      {
+        title: 'Facewash',
+        completed: false,
+      },
+      {
+        title: 'Eat',
+        completed: true,
+      },
+      {
+        title: 'Lights',
+        completed: false,
+      },
+      {
+        title: 'Pillow',
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: 'Eat',
+    color: 'green',
+    todos: [
+      {
+        title: 'Facewash',
+        completed: false,
+      },
+      {
+        title: 'Eat',
+        completed: true,
+      },
+      {
+        title: 'Lights',
+        completed: false,
+      },
+      {
+        title: 'Pillow',
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: 'Walk',
+    color: 'green',
+    todos: [
+      {
+        title: 'Facewash',
+        completed: false,
+      },
+      {
+        title: 'Eat',
+        completed: true,
+      },
+      {
+        title: 'Lights',
+        completed: false,
+      },
+      {
+        title: 'Pillow',
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: 'Blah',
+    color: 'green',
+    todos: [
+      {
+        title: 'Facewash',
+        completed: false,
+      },
+      {
+        title: 'Eat',
+        completed: true,
+      },
+      {
+        title: 'Lights',
+        completed: false,
+      },
+      {
+        title: 'Pillow',
+        completed: false,
+      },
+    ],
+  },
+];
+
+export default tempData;
